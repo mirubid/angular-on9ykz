16f747d2d57f6a3725a0351a60bb3312e59b1420
mirubid <github.10.dmil@xoxy.net>
2020-08-14 14:38:13 -0700

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,11 @@ import { ShippingComponent } from './shipping/shipping.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: '**', component: ProductListComponent},
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
+      { path: '**', component: ProductListComponent},
     ])
   ],
   declarations: [
